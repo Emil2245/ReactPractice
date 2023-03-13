5ecec71c2d764ea3be2babe21dc0c01d49df9325
Emil2245 <emilmontiel@hotmail.com>
2023-03-13 01:02:17 -0500

    Componente que actualiza la hora

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world.
-        </p>
-
+        <p>Hello world.</p>
+        <Greet/>
       </header>
     </div>
   );
---
+++ src/components/Greet.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+function Greet() { 
+  const [stateHora,setHora]=useState('Want the time?')
+  function activarHora(){
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return (setHora(time.toString()))
+  }
+  return(
+  <div>
+    <p>Hello Again Hooman!</p>
+    <p>{stateHora}</p>
+    <button onClick={() => activarHora() }>Touch this... Dare you</button>
+  </div>
+  )
+}
+
+export default Greet
