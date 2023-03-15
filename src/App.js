9844d5f8de2f3b690baad2a3aadbf6558376e9b9
Emil2245 <emilmontiel@hotmail.com>
2023-03-15 04:27:21 -0500

    componente verificacion github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Greet from './components/Greet'
+import GithubRepoChecker from './components/GithubRepoChecker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <GithubRepoChecker/>
         <p>Hello world.</p>
         <Greet/>
       </header>
